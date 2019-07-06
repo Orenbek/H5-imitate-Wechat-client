0e4d8f8467890cdd62d31bfa315690b797e2c242
乔 <qiao@lemonbox.me>
2019-07-06 13:40:39 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
@@ -15,6 +15,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: url('img/bgimg.jpg') no-repeat 50%;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  overflow: hidden;
+  display: block;
 }
 #nav {
   padding: 30px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <Login></Login> -->
+    <Users class="users"></Users>
+    <Radio class="radio"></Radio>
   </div>
 </template>
 
-<script>
+<script scoped>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Radio from '@/components/Radio.vue';
+import Login from '@/components/Login.vue';
+import Users from '@/components/Users.vue';
 
 export default {
   name: 'home',
+  http: {
+    headers: {
+      Authorization: '',
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    Radio,
+    Login,
+    Users,
+  },
+  methods: {
+    httpPost(e) {
+      console.log('hello!');
+      this.$http({
+        url: '/backend',
+        method: 'POST',
+        // 请求体重发送的数据
+        data: {
+          method: 'REG',
+          username: 'cat',
+          password: 'web-online',
+          version: '1.0',
+        },
+        // 设置请求头
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(
+        (res) => {
+          debugger;
+        },
+        (err) => {
+          debugger;
+        },
+      );
+    },
+  },
+};
 </script>
+
+<style scoped>
+.home {
+  min-width: 500px;
+  min-height: 450px;
+  height: 90%;
+  width: 70%;
+  margin: 30px auto;
+  display: block;
+  white-space: nowrap;
+}
+.users {
+  display: inline-block;
+}
+.radio {
+  display: inline-block;
+  width: calc(100% - 280px);
+}
+</style>
