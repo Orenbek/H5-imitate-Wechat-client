87baaaacfcab933d3e759842e452b7e1d95da3c3
乔 <qiao@lemonbox.me>
2019-07-11 00:55:26 +0800

    理一理思路

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -2,18 +2,40 @@
   <div class="container">
     <div class="phone-content">
       <transition-group tag="ul" class="msg-list" name="fade">
-        <li v-for="(item, index) in currentChunkList" :key="index" class="msg" @click="onPlay(index)" @touchend.prevent="onPlay(index)">
-          <img class="avatar" :src="myAvatar">
-          <div v-cloak class="audio" :style="{width: 20 * item.duration + 'px'}" :class="{wink: item.wink}">
+        <li
+          v-for="(item, index) in currentChunkList"
+          :key="index"
+          class="msg"
+          @click="onPlay(index)"
+          @touchend.prevent="onPlay(index)"
+        >
+          <img class="avatar" :src="myAvatar" />
+          <div
+            v-cloak
+            class="audio"
+            :style="{width: 20 * item.duration + 'px'}"
+            :class="{wink: item.wink}"
+          >
             <span>(</span>
             <span>(</span>
             <span>(</span>
           </div>
           <div class="duration">{{item.duration}}"</div>
         </li>
-        <li v-for="(item, index) in currentChunkList" :key="index" class="msg" @click="onPlay(index)" @touchend.prevent="onPlay(index)">
+        <li
+          v-for="(item, index) in currentChunkList"
+          :key="index"
+          class="msg"
+          @click="onPlay(index)"
+          @touchend.prevent="onPlay(index)"
+        >
           <div class="avatar1"></div>
-          <div v-cloak class="audio1" :style="{width: 20 * item.duration + 'px'}" :class="{wink: item.wink}">
+          <div
+            v-cloak
+            class="audio1"
+            :style="{width: 20 * item.duration + 'px'}"
+            :class="{wink: item.wink}"
+          >
             <span>)</span>
             <span>)</span>
             <span>)</span>
@@ -25,10 +47,20 @@
     </div>
     <div class="buttom">
       <div class="bq">
-        <div class="phone-operate" @mousedown="onMousedown" @touchstart.prevent="onMousedown"
-          @mouseup="onMouseup" @touchend.prevent="onMouseup" >{{btnText}}</div>
-        <div class="phone-operate" @mousedown="onMousedown"  @touchstart.prevent="onMousedown"
-          @mouseup="onMouseup" @touchend.prevent="onMouseup">摄像头</div>
+        <div
+          class="phone-operate"
+          @mousedown="onMousedown"
+          @touchstart.prevent="onMousedown"
+          @mouseup="onMouseup"
+          @touchend.prevent="onMouseup"
+        >{{btnText}}</div>
+        <div
+          class="phone-operate"
+          @mousedown="onMousedown"
+          @touchstart.prevent="onMousedown"
+          @mouseup="onMouseup"
+          @touchend.prevent="onMouseup"
+        >摄像头</div>
         <div class="controller">
           <img @click="onDelete" src="@/img/delete.png" alt="清除" />
           <img @click="wsSendText" style="margin: 2px 12px 0;" src="@/img/submit.png" alt="发送" />
@@ -44,6 +76,31 @@ import { onPost } from "@/services/api";
 import store from "@/store";
 var ws;
 // const ws = new WebSocket("ws://localhost:8000");
+// 遍历currentMessage
+currentMessage = [
+    {
+      index: 0,
+      mes: "",
+      userid: "我的id",
+      objectid: "",
+      type: "text",
+    },
+    {
+      index: 1,
+      mes: "",
+      userid: "对方的id",
+      objectid: "",
+      type: "audio",
+    },
+    {
+      index: 3,
+      mes: "",
+      userid: "我的id",
+      objectid: "",
+      type: "video",
+    },
+];
+// 收到消息 先要判断宿主 根据宿主ID把信息存进chatList中
 export default {
   props: {},
   components: {},
@@ -61,24 +118,24 @@ export default {
       //包含整个文字信息，包含我的和对方的
       currentNoteList: [],
       //包含当前我和当前用户之间的文字消息
-      currentChunkList:[],
+      currentChunkList: [],
       //包含当前我的当前用户之间的语音消息
       currentMessage: [],
       //包含当前我和当前用户之间的所有消息
-      choosenId: choosenId?choosenId:'',
+      choosenId: choosenId ? choosenId : ""
     };
   },
   computed: {
     getChoosenId() {
       this.choosenId = store.state.choosenId;
     },
-    getCurrentChunkList(){
+    getCurrentChunkList() {
       this.currentChunkList = this.chatList[this.choosenId];
     },
-    getCurrentNoteList(){
+    getCurrentNoteList() {
       this.currentNoteList = this.noteList[this.choosenId];
     },
-    getAvatar(){
+    getAvatar() {
       this.myAvatar = store.state.myAvatar;
     }
   },
@@ -100,25 +157,24 @@ export default {
       this.notedata = "";
     },
     onInitWs() {
-    ws = new WebSocket("ws://localhost:8000");
-    ws.addEventListener('open',this.wsOpen);
-    ws.addEventListener('message',this.wsMessage);
-    ws.addEventListener('close',this.wsClose);
-    ws.addEventListener('error',this.wsError);
-    //readyState属性返回实例对象的当前状态，共有四种。
-    //CONNECTING：值为0，表示正在连接。
-    //OPEN：值为1，表示连接成功，可以通信了。
-    //CLOSING：值为2，表示连接正在关闭。
-    //CLOSED：值为3，表示连接已经关闭，或者打开连接失败
-    //例如：if (ws.readyState == WebSocket.CONNECTING) { }
-
+      ws = new WebSocket("ws://localhost:8000");
+      ws.addEventListener("open", this.wsOpen);
+      ws.addEventListener("message", this.wsMessage);
+      ws.addEventListener("close", this.wsClose);
+      ws.addEventListener("error", this.wsError);
+      //readyState属性返回实例对象的当前状态，共有四种。
+      //CONNECTING：值为0，表示正在连接。
+      //OPEN：值为1，表示连接成功，可以通信了。
+      //CLOSING：值为2，表示连接正在关闭。
+      //CLOSED：值为3，表示连接已经关闭，或者打开连接失败
+      //例如：if (ws.readyState == WebSocket.CONNECTING) { }
     },
-    wsOpen(e){
-      console.log('connected! ',e);
+    wsOpen(e) {
+      console.log("connected! ", e);
     },
-    wsMessage(event){
+    wsMessage(event) {
       if (typeof event.data === String) {
-        console.log("Received data string",event);
+        console.log("Received data string", event);
       }
       if (event.data instanceof ArrayBuffer) {
         var buffer = event.data;
@@ -126,60 +182,60 @@ export default {
       }
       console.log("Received Message: " + event.data);
       let result = JSON.parse(event.data);
-      switch(result.type){
-        case 'audio':
+      switch (result.type) {
+        case "audio":
           this.wsReceiveAudio(result);
           break;
-        case 'video':
+        case "video":
           this.wsReceiveVideo(result);
           break;
-        case 'text':
+        case "text":
           this.wsReceiveText(result);
-          break;  
+          break;
       }
     },
-    wsClose(event){
-      console.log('已经关闭连接');
+    wsClose(event) {
+      console.log("已经关闭连接");
     },
-    wsError(){
-      console.log('连接出现问题')
+    wsError() {
+      console.log("连接出现问题");
     },
     wsSend(message) {
-      if (ws.readyState == WebSocket.OPEN) { 
+      if (ws.readyState == WebSocket.OPEN) {
         message = JSON.stringify(message);
         ws.send(message);
-      } else{
-        alert('已经断开socket连接，请重新连接websocket服务器')
+      } else {
+        alert("已经断开socket连接，请重新连接websocket服务器");
       }
     },
-    wsSendText(){
+    wsSendText() {
       let m = {
-        mes : this.notedata,
+        mes: this.notedata,
         userid: store.state.userid,
         objectid: [this.choosenId],
-        type: 'text'
-      }
-      this.wsSend(m);
-    },
-    wsSendAudio(audioStream,duration){
-      let m = {
-        mes : audioStream,
-        userid: store.state.userid,
-        objectid: [this.choosenId],
-        type: 'audio'
+        type: "text"
       };
       this.wsSend(m);
     },
-    wsReceiveText(val){
-
-      this.noteList[this.choosenId].push(val.mes);
+    wsSendAudio(audioStream, duration) {
+      let m = {
+        mes: audioStream,
+        userid: store.state.userid,
+        objectid: [this.choosenId],
+        type: "audio"
+      };
+      this.wsSend(m);
+    },
+    wsReceiveText(val) {
+      //收到的消息，应该push进发来消息对应的userid下面
+      this.noteList[val.userid].push(val.mes);
       this.currentNoteList;
     },
-    wsReceiveAudio(val){
-      console.log(val)
+    wsReceiveAudio(val) {
+      console.log(val);
     },
-    wsReceiveVideo(val){
-      console.log(val)
+    wsReceiveVideo(val) {
+      console.log(val);
     },
 
     requestAudioAccess() {
@@ -269,7 +325,7 @@ export default {
       if (duration > 60) {
         duration = 60;
       }
-      this.wsSendAudio(audioStream,duration);
+      this.wsSendAudio(audioStream, duration);
 
       let choosenId = this.choosenId;
       let chunkList = this.chatList[choosenId];
@@ -355,182 +411,182 @@ export default {
   animation: loading 1s ease-in-out infinite backwards;
 }
 .phone-content {
-	height: calc(100% - 180px);
+  height: calc(100% - 180px);
   background: #e6e6e6;
 }
 
-
 .msg-list {
-	margin: 0;
-	padding: 0;
-	height: 100%;
-	overflow-y: auto;
-	-webkit-overflow-scrolling: touch;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
 }
 .msg-list::-webkit-scrollbar {
-	display: none;
+  display: none;
 }
 .msg-list .msg {
-	list-style: none;
-	padding: 0 8px;
-	margin: 10px 0;
-	overflow: hidden;
-	cursor: pointer;
+  list-style: none;
+  padding: 0 8px;
+  margin: 10px 0;
+  overflow: hidden;
+  cursor: pointer;
 }
 .msg-list .msg .avatar,
 .msg-list .msg .audio,
 .msg-list .msg .duration {
-	float: right;
+  float: right;
 }
 .msg-list .msg .avatar1,
 .msg-list .msg .audio1,
 .msg-list .msg .duration1 {
-	float: left;
+  float: left;
 }
 .msg-list .msg .avatar {
-	width: 24px;
-	height: 24px;
-	line-height: 24px;
-	text-align: center;
-	background-color: #000;
-	/* background: url('https://denzel.netlify.com/hero.png') 0 0; */
-	background-size: 100%;
+  width: 24px;
+  height: 24px;
+  line-height: 24px;
+  text-align: center;
+  background-color: #000;
+  /* background: url('https://denzel.netlify.com/hero.png') 0 0; */
+  background-size: 100%;
 }
 .msg-list .msg .avatar1 {
-	width: 24px;
-	height: 24px;
-	line-height: 24px;
-	text-align: center;
-	background-color: #000;
-	background: url('https://denzel.netlify.com/hero.png') 0 0;
-	background-size: 100%;
+  width: 24px;
+  height: 24px;
+  line-height: 24px;
+  text-align: center;
+  background-color: #000;
+  background: url("https://denzel.netlify.com/hero.png") 0 0;
+  background-size: 100%;
 }
 .msg-list .msg .audio {
-	position: relative;
-	margin-right: 6px;
-	max-width: 116px;
-	min-width: 30px;
-	height: 24px;
-	line-height: 24px;
-	padding: 0 4px 0 10px;
-	border-radius: 2px;
-	color: #000;
-	text-align: right;
-	background-color: rgba(107, 197, 107, 0.85);
+  position: relative;
+  margin-right: 6px;
+  max-width: 116px;
+  min-width: 30px;
+  height: 24px;
+  line-height: 24px;
+  padding: 0 4px 0 10px;
+  border-radius: 2px;
+  color: #000;
+  text-align: right;
+  background-color: rgba(107, 197, 107, 0.85);
 }
 .msg-list .msg .audio1 {
-	position: relative;
-	margin-left: 6px;
-	max-width: 116px;
-	min-width: 30px;
-	height: 24px;
-	line-height: 24px;
-	padding: 0 4px 0 10px;
-	border-radius: 2px;
-	color: #000;
-	text-align: left;
-	background-color: rgba(194, 194, 194, 0.678);
+  position: relative;
+  margin-left: 6px;
+  max-width: 116px;
+  min-width: 30px;
+  height: 24px;
+  line-height: 24px;
+  padding: 0 4px 0 10px;
+  border-radius: 2px;
+  color: #000;
+  text-align: left;
+  background-color: rgba(194, 194, 194, 0.678);
 }
 .msg-list .msg.eg {
-	cursor: default;
+  cursor: default;
 }
 .msg-list .msg.eg .audio {
-	text-align: left;
+  text-align: left;
 }
 .msg-list .msg .audio:before {
-	position: absolute;
-	right: -8px;
-	top: 8px;
-	content: '';
-	display: inline-block;
-	width: 0;
-	height: 0;
-	border-style: solid;
-	border-width: 4px;
-	border-color: transparent transparent transparent rgba(107, 197, 107, 0.85);
+  position: absolute;
+  right: -8px;
+  top: 8px;
+  content: "";
+  display: inline-block;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 4px;
+  border-color: transparent transparent transparent rgba(107, 197, 107, 0.85);
 }
 .msg-list .msg .audio span {
-	color: rgba(255, 255, 255, .8);
-	display: inline-block;
-	transform-origin: center;
+  color: rgba(255, 255, 255, 0.8);
+  display: inline-block;
+  transform-origin: center;
 }
 .msg-list .msg .audio span:nth-child(1) {
-	font-weight: 400;
+  font-weight: 400;
 }
 .msg-list .msg .audio span:nth-child(2) {
-	transform: scale(0.8);
-	font-weight: 500;
+  transform: scale(0.8);
+  font-weight: 500;
 }
 .msg-list .msg .audio span:nth-child(3) {
-	transform: scale(0.5);
-	font-weight: 700
+  transform: scale(0.5);
+  font-weight: 700;
 }
 .msg-list .msg .audio.wink span {
-	animation: wink 1s ease infinite;
+  animation: wink 1s ease infinite;
 }
-
 
 .msg-list .msg.eg .audio1 {
-	text-align: left;
+  text-align: left;
 }
 .msg-list .msg .audio1:before {
-	position: absolute;
-	left: -8px;
-	top: 8px;
-	content: '';
-	display: inline-block;
-	width: 0;
-	height: 0;
-	border-style: solid;
-	border-width: 4px;
-	border-color: transparent rgba(194, 194, 194, 0.678) transparent transparent;
+  position: absolute;
+  left: -8px;
+  top: 8px;
+  content: "";
+  display: inline-block;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-width: 4px;
+  border-color: transparent rgba(194, 194, 194, 0.678) transparent transparent;
 }
 .msg-list .msg .audio1 span {
-	color: rgba(255, 255, 255, .8);
-	display: inline-block;
-	transform-origin: center;
+  color: rgba(255, 255, 255, 0.8);
+  display: inline-block;
+  transform-origin: center;
 }
 .msg-list .msg .audio1 span:nth-child(3) {
-	font-weight: 400;
+  font-weight: 400;
 }
 .msg-list .msg .audio1 span:nth-child(2) {
-	transform: scale(0.8);
-	font-weight: 500;
+  transform: scale(0.8);
+  font-weight: 500;
 }
 .msg-list .msg .audio1 span:nth-child(1) {
-	transform: scale(0.5);
-	font-weight: 700
+  transform: scale(0.5);
+  font-weight: 700;
 }
 .msg-list .msg .audio1.wink span {
-	animation: wink 1s ease infinite;
+  animation: wink 1s ease infinite;
 }
 
 .msg-list .msg .duration {
-	margin: 3px 2px;
+  margin: 3px 2px;
 }
 .msg-list .msg .duration1 {
-	margin: 3px 2px;
+  margin: 3px 2px;
 }
-.fade-enter-active, .fade-leave-active {
-	transition: opacity .5s;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
 }
-.fade-enter, .fade-leave-to {
-	opacity: 0;
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 @keyframes wink {
-	from {
-		color: rgba(255, 255, 255, .8);
-	}
-	to {
-		color: rgba(255, 255, 255, .1);
-	}
+  from {
+    color: rgba(255, 255, 255, 0.8);
+  }
+  to {
+    color: rgba(255, 255, 255, 0.1);
+  }
 }
 @keyframes loading {
-	from {
-		width: 0%;
-	}
-	to {
-		width: 100%;
-	}
+  from {
+    width: 0%;
+  }
+  to {
+    width: 100%;
+  }
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     mqttServ: 'mqtt://10.112.163.194',
+    wsHost: "ws://localhost:8000",
     choosenId:'390363',
     myAvatar: '',
     session: '',
